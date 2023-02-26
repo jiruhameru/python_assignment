301fd16c5b9236cde1af9eb0fb71cf8513f24986
jiruhameru <ZAlHamer@gmail.com>
2023-02-27 05:07:43 +0900

    Added API code

---
+++ financial/get_financial_data API.py
@@ -0,0 +1,86 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+from datetime import datetime
+import os 
+from dotenv import load_dotenv
+
+app = Flask(__name__)
+api = Api(app)
+
+class FinancialData(Resource):
+    def get(self):
+        args = request.args
+        start_date = args.get('start_date')
+        end_date = args.get('end_date')
+        symbol = args.get('symbol')
+        limit = args.get('limit')
+        page = args.get('page')
+
+
+        result = None
+        error = ''
+
+        try:
+            conn = psycopg2.connect(host=os.environ.get('PG_HOST'),
+                            port=os.environ.get('PG_PORT'),
+                            user=os.environ.get('PG_USER'),
+                            password=os.environ.get('PG_PASSWORD'),
+                            dbname=os.environ.get('PG_DATABASE'))
+
+
+            cursor = conn.cursor()
+                
+            if (limit is None):
+                limit = 5
+            if (page is None):
+                page = 1
+            offset = int(page) * int(limit)
+            if(symbol is None):                
+                sql_query = "select row_to_json(t) as obj from (SELECT symbol, date, open_price, close_price, volume FROM financial_data LIMIT %s OFFSET %s) t;"
+                cursor.execute(sql_query, (limit, offset))                
+            elif (start_date is None or end_date is None):
+                sql_query = "select row_to_json(t) as obj from (SELECT symbol, date, open_price, close_price, volume FROM financial_data WHERE symbol=%s LIMIT %s OFFSET %s) t;"
+                cursor.execute(sql_query, (symbol, limit, offset))
+            elif None not in (symbol,start_date,end_date, limit, offset): 
+                sql_query = "select row_to_json(t) as obj from (SELECT symbol, date, open_price, close_price, volume FROM financial_data WHERE symbol=%s AND date BETWEEN %s AND %s LIMIT %s OFFSET %s) t;"
+                cursor.execute(sql_query, (symbol,start_date,end_date, limit, offset))
+            
+            rows = cursor.fetchall()                            
+
+            cursor_all = conn.cursor()
+            sql_query_count = "select row_to_json(t) as obj from (SELECT symbol, date, open_price, close_price, volume FROM financial_data WHERE symbol=%s AND date BETWEEN %s AND %s) t;"
+            cursor_all.execute(sql_query_count, (symbol,start_date,end_date))            
+
+            count = cursor_all.rowcount            
+            pages = round((count / int(limit)), 0)
+
+            l1 = [x[0] for x in rows]
+            pagination_d = {'count': count, 'page': page, 'limit': limit, 'pages': pages}            
+
+        except (Exception, psycopg2.DatabaseError) as e:
+            error = e
+        finally:
+            info_d = {'error': error}
+            d = {'data': l1, 'pagination': pagination_d, 'info': info_d}
+                            
+            result = d                
+            if conn is not None:
+                conn.close()    
+
+        return result
+
+
+api.add_resource(FinancialData, '/api/financial_data')
+
+# Running the Local Server
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
+
+
+# http://localhost:5000/api/financial_data?start_date=2023-02-10&end_date=2023-02-20&symbol=IBM&limit=10&page=9
+
---
+++ financial/get_statisics_API.py
@@ -0,0 +1,95 @@
+from flask import Flask, request
+from flask_restful import Resource, Api, reqparse
+import pandas as pd
+
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+from datetime import datetime
+import numpy as np
+import math
+
+
+app = Flask(__name__)
+api = Api(app)
+
+class Statistics(Resource):
+    def get(self):
+        args = request.args
+        start_date = args.get('start_date')
+        end_date = args.get('end_date')
+        symbol = args.get('symbol')
+
+        result = None
+        error = ''
+        
+        try:
+            conn_string = "host='localhost' dbname='local_db' user='postgres' password='locpass'"
+            conn = psycopg2.connect(conn_string)
+
+            cursor = conn.cursor()
+                
+            if None not in (start_date, end_date, symbol):
+                    
+                # get the average daily open price resuts:
+                average_daily_open_price_sql_query = "select row_to_json(t) as obj from (SELECT AVG(open_price) FROM financial_data WHERE symbol=%s AND date BETWEEN %s AND %s GROUP BY date) t;"                
+                cursor.execute(average_daily_open_price_sql_query, (symbol,start_date,end_date))
+            
+                average_daily_open_price_rows = cursor.fetchall()                
+                l1 = [x[0] for x in average_daily_open_price_rows]
+                open_price_daily_averages = [av['avg'] for av in l1] #get the average values for each day                
+                open_price_daily_average = np.round(np.mean(open_price_daily_averages), 2) #get the total daily average (scalar)
+                if math.isnan(open_price_daily_average):
+                    open_price_daily_average = 0
+                
+
+                # get the average daily close price resuts
+                average_daily_close_price_sql_query = "select row_to_json(t) as obj from (SELECT AVG(close_price) FROM financial_data WHERE symbol=%s AND date BETWEEN %s AND %s GROUP BY date) t;"                
+                cursor.execute(average_daily_close_price_sql_query, (symbol,start_date,end_date))
+
+                average_daily_close_price_rows = cursor.fetchall()                
+                l1 = [x[0] for x in average_daily_close_price_rows]
+                close_price_daily_averages = [av['avg'] for av in l1] #get the average values for each day                
+                close_price_daily_average = np.round(np.mean(close_price_daily_averages), 2) #get the total daily average (scalar)            
+                if math.isnan(close_price_daily_average):
+                    close_price_daily_average = 0
+
+                # get the average daily volume resuts
+                average_daily_volume_sql_query = "select row_to_json(t) as obj from (SELECT AVG(volume) FROM financial_data WHERE symbol=%s AND date BETWEEN %s AND %s GROUP BY date) t;"                
+                cursor.execute(average_daily_volume_sql_query, (symbol,start_date,end_date))
+
+                average_daily_volume_rows = cursor.fetchall()                
+                l1 = [x[0] for x in average_daily_volume_rows]
+                volume_daily_averages = [av['avg'] for av in l1] #get the average values for each day                
+                volume_daily_average = np.round(np.mean(volume_daily_averages), 2) #get the total daily average (scalar)
+                if math.isnan(volume_daily_average):
+                    volume_daily_average = 0
+
+                stat_dict = {
+                    'start_date': start_date,
+                    'end_date': end_date,
+                    'symbol': symbol, 
+                    'average_daily_open_price': open_price_daily_average,
+                    'average_daily_close_price': close_price_daily_average,
+                    'average_daily_volume': volume_daily_average}            
+                info_d = {'error': error}
+                metdadata = {'data': stat_dict, 'info': info_d}
+                                
+                result = metdadata
+        except (Exception, psycopg2.DatabaseError) as e:
+                error = e
+        finally:            
+            if conn is not None:
+                conn.close()    
+
+        return result
+
+api.add_resource(Statistics, '/api/statistics')
+
+# Running the Local Server
+if __name__ == '__main__':
+    app.run(host='localhost', port=5000)
+
+
+# http://localhost:5000/api/statistics?start_date=2023-02-10&end_date=2023-02-20&symbol=IBM
+
